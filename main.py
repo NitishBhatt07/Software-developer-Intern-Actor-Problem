5b24d8994763d0e544c119aed277ab35a5293800
Nitish Bhatt <nbbhatt07@gmail.com>
2021-12-13 19:41:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,51 +21,29 @@ def ret_movie_list(movies):
         for j in range(i+1,len(movies)):
             
             # month match.......................
-            if movies[i]['start_date'].split()[1] == movies[j]['start_date'].split()[1] and movies[i]['end_date'].split()[1] == movies[j]['end_date'].split()[1]:
-                #print(movies[i]['movie_name'],movies[j]['movie_name']+" ---> "+"month matched")
-                
-                if ( int(movies[i]['start_date'].split()[0]) > int(movies[j]['start_date'].split()[0]) ):
-                    #print(movies[i]['movie_name'],movies[j]['movie_name']+" ---> "+" i movie start after j movie", "..............Done")
-                    #seq.append({movies[i]['movie_name']:movies[j]['movie_name']})
-                    
+            if movies[i]['start_date'].split()[1] == movies[j]['start_date'].split()[1] and movies[i]['end_date'].split()[1] == movies[j]['end_date'].split()[1]:                
+                if ( int(movies[i]['start_date'].split()[0]) > int(movies[j]['start_date'].split()[0]) ):           
                     if movies[i]['movie_name'] not in temp:
                         temp[movies[i]['movie_name']] = [movies[j]['movie_name']]
                     else:
                         temp[movies[i]['movie_name']].append(movies[j]['movie_name'])
                     
-                elif ( int(movies[i]['end_date'].split()[0]) < int(movies[j]['start_date'].split()[0]) ):
-                    #print(movies[i]['movie_name'],movies[j]['movie_name']+" ---> "+" i movie end before j movie", "..............Done")
-                    #seq.append({movies[i]['movie_name']:movies[j]['movie_name']})  
-                    
+                elif ( int(movies[i]['end_date'].split()[0]) < int(movies[j]['start_date'].split()[0]) ):                    
                     if movies[i]['movie_name'] not in temp:
                         temp[movies[i]['movie_name']] = [movies[j]['movie_name']]
                     else:
                         temp[movies[i]['movie_name']].append(movies[j]['movie_name'])
-                     
-                else:
-                    pass
-                    #print(movies[i]['movie_name'],movies[j]['movie_name']+" ---> "+" i and j movie overlap each other")
             
             # only start month match , end month not match
             elif movies[i]['start_date'].split()[1] == movies[j]['start_date'].split()[1] and movies[i]['end_date'].split()[1] != movies[j]['end_date'].split()[1]:
-                #print(movies[i]['movie_name'],movies[j]['movie_name']+" ---> ","start match end not match")
-                if ( int(movies[i]['end_date'].split()[0]) < int(movies[j]['start_date'].split()[0]) ):
-                    #print(movies[i]['movie_name'],movies[j]['movie_name']+" ---> "+" i movie finish befor j movie", ".............Done")
-                    #seq.append({movies[i]['movie_name']:movies[j]['movie_name']})
-                    
+                if ( int(movies[i]['end_date'].split()[0]) < int(movies[j]['start_date'].split()[0]) ):                    
                     if movies[i]['movie_name'] not in temp:
                         temp[movies[i]['movie_name']] = [movies[j]['movie_name']]
                     else:
                         temp[movies[i]['movie_name']].append(movies[j]['movie_name'])
-                else:
-                    pass
-                    #print(movies[i]['movie_name'],movies[j]['movie_name']+" ---> "+" i and j movie overlap each other")
             
             # no start end month matches
-            elif movies[i]['start_date'].split()[1] != movies[j]['start_date'].split()[1] and movies[i]['end_date'].split()[1] != movies[j]['end_date'].split()[1]:
-                #print(movies[i]['movie_name'],movies[j]['movie_name']+" --> "+"no month matches", "..........................Done")
-                #seq.append({movies[i]['movie_name']:movies[j]['movie_name']})
-                
+            elif movies[i]['start_date'].split()[1] != movies[j]['start_date'].split()[1] and movies[i]['end_date'].split()[1] != movies[j]['end_date'].split()[1]:                
                 if movies[i]['movie_name'] not in temp:
                         temp[movies[i]['movie_name']] = [movies[j]['movie_name']]
                 else:
@@ -81,7 +59,6 @@ def ret_movie_list(movies):
 
 def get_sequence(temp,new_movie_info):
     temp2 = {}
-
     length = 0
     li = []
     main_seq = []
@@ -90,50 +67,38 @@ def get_sequence(temp,new_movie_info):
         for i in range(0,len(temp[key])-1):
 
             # month match.......................
-            if new_movie_info[temp[key][i]]['start_date'].split()[1] == new_movie_info[temp[key][i+1]]['start_date'].split()[1] and new_movie_info[temp[key][i]]['end_date'].split()[1] == new_movie_info[temp[key][i+1]]['end_date'].split()[1]:
-                #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"month match..................")
-
-                if ( int(new_movie_info[temp[key][i]]['start_date'].split()[0]) > int(new_movie_info[temp[key][i+1]]['start_date'].split()[0]) ):
-                    #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"i movie start after j movie..............Done")
-                    
+            if new_movie_info[temp[key][i]]['start_date'].split()[1] == new_movie_info[temp[key][i+1]]['start_date'].split()[1] and\
+            new_movie_info[temp[key][i]]['end_date'].split()[1] == new_movie_info[temp[key][i+1]]['end_date'].split()[1]:
+                
+                if ( int(new_movie_info[temp[key][i]]['start_date'].split()[0]) > int(new_movie_info[temp[key][i+1]]['start_date'].split()[0]) ):                    
                     if key not in temp2:
                         temp2[key] = [temp[key][i],temp[key][i+1]]
                     else:
                         temp2[key].append(temp[key][i])
                         temp2[key].append(temp[key][i+1])
                         
-                elif ( int(new_movie_info[temp[key][i]]['end_date'].split()[0]) < int(new_movie_info[temp[key][i+1]]['start_date'].split()[0]) ):
-                    #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"i movie end before j movie..................Done")
-                    
+                elif ( int(new_movie_info[temp[key][i]]['end_date'].split()[0]) < int(new_movie_info[temp[key][i+1]]['start_date'].split()[0]) ):                    
                     if key not in temp2:
                         temp2[key] = [temp[key][i],temp[key][i+1]]
                     else:
                         temp2[key].append(temp[key][i])
                         temp2[key].append(temp[key][i+1])
-                else:
-                    pass
-                    #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"i and j movie overlap each other")
 
             # only start month match , end month not match
-            elif new_movie_info[temp[key][i]]['start_date'].split()[1] == new_movie_info[temp[key][i+1]]['start_date'].split()[1] and new_movie_info[temp[key][i]]['end_date'].split()[1] != new_movie_info[temp[key][i+1]]['end_date'].split()[1]:
-                #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"start match end not match")
+            elif new_movie_info[temp[key][i]]['start_date'].split()[1] == new_movie_info[temp[key][i+1]]['start_date'].split()[1] and\
+            new_movie_info[temp[key][i]]['end_date'].split()[1] != new_movie_info[temp[key][i+1]]['end_date'].split()[1]:
 
-                if ( int(new_movie_info[temp[key][i]]['end_date'].split()[0]) < int(new_movie_info[temp[key][i+1]]['start_date'].split()[0]) ):
-                    #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"i movie finish befor j movie.....DOne")
-                    
+                if ( int(new_movie_info[temp[key][i]]['end_date'].split()[0]) < int(new_movie_info[temp[key][i+1]]['start_date'].split()[0]) ):                    
                     if key not in temp2:
                         temp2[key] = [temp[key][i],temp[key][i+1]]
                     else:
                         temp2[key].append(temp[key][i])
                         temp2[key].append(temp[key][i+1])
-                else:
-                    pass
-                    #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"i and j movie overlap each other")
 
             # no start end month matches
-            elif new_movie_info[temp[key][i]]['start_date'].split()[1] != new_movie_info[temp[key][i+1]]['start_date'].split()[1] and new_movie_info[temp[key][i]]['end_date'].split()[1] != new_movie_info[temp[key][i+1]]['end_date'].split()[1]:
-                #print(key,new_movie_info[temp[key][i]],new_movie_info[temp[key][i+1]],"no month matches...........")
-                
+            elif new_movie_info[temp[key][i]]['start_date'].split()[1] != new_movie_info[temp[key][i+1]]['start_date'].split()[1] and\
+            new_movie_info[temp[key][i]]['end_date'].split()[1] != new_movie_info[temp[key][i+1]]['end_date'].split()[1]:
+            
                 if key not in temp2:
                     temp2[key] = [temp[key][i],temp[key][i+1]]
                 else:
@@ -154,8 +119,7 @@ def get_sequence(temp,new_movie_info):
 
 
 if __name__ == "__main__":
-   
-
+    
     # Test Case 1
     movie_info = [ {'movie_name':'Bala',"start_date":'08 jan',"end_date":'28 jan'},
     {'movie_name':'Rock',"start_date":'20 jan',"end_date":'30 jan'},
@@ -184,7 +148,3 @@ if __name__ == "__main__":
     new_movie_info2 = get_indexed_dict(movie_info3)
     seq2, money2 = ret_movie_list(new_movie_info2)
     print(f"Test Case 2:\nsequence: {seq2}\nmoney: {money2} crore")
-
-
-    
-    
